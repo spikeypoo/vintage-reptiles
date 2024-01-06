02c2493a0cd8148c6f781c717cdeda4e29267479
Spike <skinneth123@gmail.com>
2024-01-06 00:12:32 -0500

    chicken butt!!!!

--- dataGrab.js
+++ dataGrab.js
@@ -14,6 +14,8 @@ const firebaseConfig = {
 const reptilesCollection = firebase.firestore().collection('geckos');
 const reptileContainer = document.getElementById('reptileContainer');
 
+
+
 reptilesCollection.get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
         const data = doc.data();
@@ -23,10 +25,35 @@ reptilesCollection.get().then((querySnapshot) => {
 
         reptileCard.innerHTML = `
             <h2 class="header" style="text-decoration:none">${data.name}</h2>
-            <img style="margin-left:auto;margin-right:auto;display:block" src="${data.gecko}" alt="${data.name}">
-        `;
+            <img style="margin-left:auto;margin-right:auto;display:block;cursor:pointer;" 
+                src="${data.gecko}" alt="${data.name}" 
+                onclick="openModal('${data.name}', '${data.gecko}', '${data.description}', '${data.price}', '${data.gp1}', '${data.gp2}')">
+            `;
 
         // Append the reptile card to the container
         reptileContainer.appendChild(reptileCard);
     });
 });
+
+function openModal(name, image, desc, price, gp1, gp2) {
+    const modalImages = document.getElementById('modalImages');
+    const modalContent = document.getElementById('modalContent');
+    modalImages.innerHTML = `
+    <img src="${image}" alt="${name}" style="width:500px;border:5px solid white">
+    `;
+    modalContent.innerHTML = `
+      <h2 class="header2" style="text-decoration:none;padding-left:100px;padding-bottom:0;font-size:30px">${name}<span class="pg" style="margin-top:100px;font-size:20px"><br>${desc}<br><br>$${price}</span></h2>
+      <img src="${image}" alt="${name}" style="width:100px;position:relative;right:435px;top:210px;margin-right:10px;border:5px solid white">
+      <img src="${image}" alt="${name}" style="width:100px;position:relative;right:435px;top:210px;margin-right:10px;border:5px solid white">
+      <img src="${image}" alt="${name}" style="width:100px;position:relative;right:435px;top:210px;border:5px solid white">
+      <!-- Add more details if needed -->
+    `;
+    const modal = document.getElementById('geckoModal');
+    modal.style.display = 'block';
+  }
+  
+  // Function to close the modal
+  function closeModal() {
+    const modal = document.getElementById('geckoModal');
+    modal.style.display = 'none';
+  }
